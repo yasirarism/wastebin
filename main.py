3b2ec05554bb4c4c100a035a0e4e9242c0456ccf
yasirarism <55983182+yasirarism@users.noreply.github.com>
2023-03-08 11:51:10 +0700

    Update main.py

--- main.py
+++ main.py
@@ -71,7 +71,7 @@ async def view(id: str, request: Request):
 @app.get('/mediainfo/{id:path}', response_class=HTMLResponse)
 async def view_mediainfo(id: str, request: Request):
     document = await api_get(id)
-    return templates.TemplateResponse('view.html', {'request': request, 'document': document})
+    return templates.TemplateResponse('mediainfo.html', {'request': request, 'document': document})
 
 
 @app.exception_handler(404)
